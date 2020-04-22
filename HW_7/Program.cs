ed297fe154f3ea281c296c633b66367f1ed0b9cf
KiselevAndrey <KiselevAndrey@users.noreply.github.com>
2020-04-22 10:09:49 +0300

    fix hiden HW_7

---
+++ HW_7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Kiselev_Andrey;
+
+namespace HW_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            bool print = true;
+
+            Student st1 = new Student("Andrey", print);
+            st1.AddGroup("p-47", print);
+            st1.AddMark(9, print);
+            st1.AddMark(7, print);
+            st1.AddMark(8, print);
+            st1.AddMark(9, print);
+
+            st1.Print();
+
+            Console.WriteLine("Now student's group");
+            StartMenu.EnterClearConsole();
+
+            while (true)
+            {
+                byte choice = StartMenu.Choiсe("Student's group", ");
+
+                StartMenu.EnterClearConsole();
+            }
+        }
+    }
+}
