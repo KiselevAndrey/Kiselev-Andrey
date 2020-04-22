f44a432652b1b101318d42355e319cdd8d839062
KiselevAndrey <KiselevAndrey@users.noreply.github.com>
2020-04-22 11:10:52 +0300

    create HW_11

---
+++ HW_11/HeadTeacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_11
+{
+    public class HeadTeacher : Teacher
+    {
+        public HeadTeacher(string name, string surname, string departament) : base(name, surname, departament) { }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string ToString()
+        {
+            return $"My name is {Name} {Surname}.\tI'm Head Teacher in facultet {Departament}";
+        }
+    }
+}
---
+++ HW_11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HW_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[5]
+            {
+                new Teacher("Ярослав", "Беляк", "Абстракции"),
+                new Student("Кирилл", "Туровский", "Княжеводчества"),
+                new Teacher("Сабина", "Бабаева", "Аудита"),
+                new Student("Кирилл", "Туровский", "Княжеводчества"),
+                new HeadTeacher("Ярослав", "Беляк", "Абстракции"),
+            };
+
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i]);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < persons.Length - 1; i++)
+            {
+                for (int j = i+1; j < persons.Length; j++)
+                {
+                    Console.WriteLine(persons[i]);
+                    Console.WriteLine(persons[j]);
+                    Console.WriteLine("We are " + (persons[i].Equals(persons[j]) ? "" : "don't ") + "equals.\n");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
